bb485902418977f65da7ad3e6cec7cd1a6e25562
Eduardo Campos Jiménez <educamjz@gmail.com>
2023-04-03 15:12:22 +0000

    Version 1.1

--- p2s-vpn-Linux/deploy-vnp-network-file-share.sh
+++ p2s-vpn-Linux/deploy-vnp-network-file-share.sh
@@ -1,14 +1,41 @@
+# Variables
+installDir="/etc/"
 region="westeurope"
 resourceGroupName="RG01"
 virtualNetworkName="vNetRG01"
 vpnName="vNetGwRG01"
 publicIpAddressName="$vpnName-PublicIP"
 rootCertName="P2SRootCertLinux"
+username="client"
+password="12345678"
 rootCertificate="./rootCert.pem"
 mystorageaccount="saecj00"
 myfileshare="data-ecj"
+mystorageaccount="saecj00"
+myfileshare="data-ecj"
+myPrivateEndPoint="pep-ecj"
 
-az login
+# Create CA
+ipsec pki --gen --outform pem > rootKey.pem
+ipsec pki --self --in rootKey.pem --dn "CN=$rootCertName" --ca --outform pem > rootCert.pem
+
+rootCertificate=$(openssl x509 -in rootCert.pem -outform der | base64 -w0 ; echo)
+
+ipsec pki --gen --size 4096 --outform pem > "clientKey.pem"
+ipsec pki --pub --in "clientKey.pem" | \
+    ipsec pki \
+        --issue \
+        --cacert rootCert.pem \
+        --cakey rootKey.pem \
+        --dn "CN=$username" \
+        --san $username \
+        --flag clientAuth \
+        --outform pem > "clientCert.pem"
+
+openssl pkcs12 -in "clientCert.pem" -inkey "clientKey.pem" -certfile rootCert.pem -export -out "$username.p12" -password "pass:$password"
+
+# Login to the Azure account
+az login --use-device-code
 
 echo "Creating ResourceGroup"
 az group create -l $region -n $resourceGroupName
@@ -101,10 +128,81 @@ vpnClient=$(az network vnet-gateway vpn-client generate \
     --authentication-method EAPTLS | tr -d '"')
 
 curl $vpnClient --output vpnClient.zip
-#echo " --------------------------------"
-#echo ""
+echo " --------------------------------"
+echo ""
 
-# echo "Creating Storage Account and File Share"
-# az storage account create -n $mystorageaccount -g $resourceGroupName -l $region --sku Standard_GRS
-#storage_conn_string=$(az storage account show-connection-string -n $mystorageaccount -g $resourceGroupName --query 'connectionString' -o tsv)
-#az storage share create --name $myfileshare --connection-string $storage_conn_string
+echo "Deploying VPN"
+unzip vpnClient.zip
+
+vpnServer=$(xmllint --xpath "string(/VpnProfile/VpnServer)" Generic/VpnSettings.xml)
+vpnType=$(xmllint --xpath "string(/VpnProfile/VpnType)" Generic/VpnSettings.xml | tr '[:upper:]' '[:lower:]')
+routes=$(xmllint --xpath "string(/VpnProfile/Routes)" Generic/VpnSettings.xml)
+
+cp "${installDir}ipsec.conf" "${installDir}ipsec.conf.backup"
+cp "Generic/VpnServerRoot.cer_0" "${installDir}ipsec.d/cacerts"
+cp "${username}.p12" "${installDir}ipsec.d/private" 
+
+tee -a "${installDir}ipsec.conf" <<EOF
+conn azure
+    keyexchange=$vpnType
+    type=tunnel
+    leftfirewall=yes
+    left=%any
+    leftauth=eap-tls
+    leftid=%client
+    right=$vpnServer
+    rightid=%$vpnServer
+    rightsubnet=$routes
+    leftsourceip=%config
+    esp=aes256gcm16,aes128gcm16!
+    auto=add
+EOF
+
+echo ": P12 $username.p12 '$password'" | sudo tee -a "${installDir}ipsec.secrets" > /dev/null
+echo " --------------------------------"
+echo ""
+
+
+echo "Connecting VPN"
+# Restar VPN
+ipsec restart
+
+# Up tunel "azure"
+ipsec up azure
+echo " --------------------------------"
+echo ""
+
+echo "Creating Storasge Account"
+az storage account create -n $mystorageaccount -g $resourceGroupName -l $region --sku Standard_GRS --default-action Deny
+echo " --------------------------------"
+echo ""
+
+echo "Allowing access from my public IP to storager account"
+az storage account network-rule add --resource-group $resourceGroupName --account-name $mystorageaccount --ip-address $my_public_IP
+echo " --------------------------------"
+echo ""
+
+echo "Wait ..."
+sleep 30
+echo " --------------------------------"
+echo ""
+
+echo "Creating File Share Storage Account"
+storage_conn_string=$(az storage account show-connection-string -n $mystorageaccount -g $resourceGroupName --query 'connectionString' -o tsv)
+az storage share create --name $myfileshare --connection-string $storage_conn_string
+echo " --------------------------------"
+echo ""
+
+echo "Creating Private Point Storage Account"
+storage_id=$(az storage account show --resource-group $resourceGroupName --name $mystorageaccount | jq -r .id)
+
+az network private-endpoint create \
+    --name $myPrivateEndPoint \
+    --resource-group $resourceGroupName  \
+    --vnet-name $virtualNetworkName \
+    --subnet "PrivateEndpointSubnet" \
+    --private-connection-resource-id $storage_id \
+    --group-id file \
+    --connection-name "pep-ecj-nic"
+echo " --------------------------------"
+echo ""
