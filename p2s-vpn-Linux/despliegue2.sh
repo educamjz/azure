178330775acacd52a7e684faffd833caf8a99eec
Eduardo Campos Jiménez <educamjz@gmail.com>
2023-04-04 20:11:24 +0000

    Versio 1.7

--- p2s-vpn-Linux/despliegue2.sh
+++ p2s-vpn-Linux/despliegue2.sh
@@ -12,6 +12,30 @@ mystorageaccount="saecj00"
 myfileshare="data-ecj"
 myPrivateEndPoint="pep-ecj"
 
+# Create CA
+echo "Creating CA"
+sudo ipsec pki --gen --outform pem > rootKey.pem
+sudo ipsec pki --self --in rootKey.pem --dn "CN=$rootCertName" --ca --outform pem > rootCert.pem
+
+rootCertificate=$(openssl x509 -in rootCert.pem -outform der | base64 -w0 ; echo)
+
+sudo ipsec pki --gen --size 4096 --outform pem > "clientKey.pem"
+sudo ipsec pki --pub --in "clientKey.pem" | \
+    sudo ipsec pki \
+        --issue \
+        --cacert rootCert.pem \
+        --cakey rootKey.pem \
+        --dn "CN=$username" \
+        --san $username \
+        --flag clientAuth \
+        --outform pem > "clientCert.pem"
+
+openssl pkcs12 -in "clientCert.pem" -inkey "clientKey.pem" -certfile rootCert.pem -export -out "$username.p12" -password "pass:$password"
+
+Wait ...
+sleep 30
+echo " --------------------------------"
+
 # Login to the Azure account
 az login --use-device-code
 
